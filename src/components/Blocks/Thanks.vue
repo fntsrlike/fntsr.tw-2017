1707fb99e07e04843a67fd885ad4aef40cd09fcd
Ruoshi Lin <me@fntsr.tw>
2017-11-20 20:55:00 +0800

    Improve "Thanks" section

--- src/components/Blocks/Thanks.vue
+++ src/components/Blocks/Thanks.vue
@@ -1,13 +1,14 @@
 <template>
   <section class="block">
     <section>
-      <p>Thanks</p>
+      <h2>Thanks</h2>
       <p>
-        The font is Noto Sans, provide by Google. The background is provided by <a href="http://subtlepatterns.com/">Subtle Patterns</a>.
-        I love paper texture background so much! =D
+        The font is <a href="https://www.google.com/get/noto/">Noto Sans</a>, provide by <a href="https://fonts.google.com">Google Fonts</a>.
+        The background is provided by <a href="http://subtlepatterns.com/">Subtle Patterns</a>.
+        This website is built with <a href="https://vuejs.org">Vue.js</a> and host on <a href="https://pages.github.com">GitHub Pages</a>.
       </p>
     </section>
-  </section>  
+  </section>
 </template>
 
 <script>
