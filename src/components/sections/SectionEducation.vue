a51fcb9de75ed95d3651ed9a30aa8afb547f19b4
Ruoshi Lin <me@fntsr.tw>
2017-11-26 23:29:21 +0800

    Improve education section

--- src/components/sections/SectionEducation.vue
+++ src/components/sections/SectionEducation.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="block">
     <h2>Education</h2>
-    I have been studying history, but also learning programming. After graduating from university, I study software engineering.
     <ul>
       <li>
-        Master's of <strong>Software Engineering</strong>, <a target="_blank" href="http://www.ncu.edu.tw/">NCU</a> (2014-2016)
+        Master's of <strong>Software Engineering</strong>, <a target="_blank" href="http://www.ncu.edu.tw/">NCU</a> (2014-2016)<br/>
+        <small>Thesis: "Visualize Ripple Effect with Analyzing Object-Oriented Design Relationship"</small>
       </li>
       <li>
         Bachelor of <strong>History</strong>, <a target="_blank" href="http://www.nchu.edu.tw/">NCHU</a> (2010-2014)
