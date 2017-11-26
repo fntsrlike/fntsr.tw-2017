a34165e4a59c5f7234d6cce54cdbc4739fff22e7
Ruoshi Lin <me@fntsr.tw>
2017-11-26 23:41:47 +0800

    Merge content of Projects section to Collections section

--- src/components/sections/SectionProjects.vue
+++ src/components/sections/SectionProjects.vue
@@ -1,90 +1,9 @@
 <template>
   <section class="block">
     <h2>Projects</h2>
-    <section>
-      <p>
-        Except above, I also take the cases and develop by myself. I create some personal projects for intresting and practice skill, too.
-      </p>
-      <ul>
-        <li>
-          Ninde Grid Judge System
-        </li>
-        <li>
-          Prototye of MeDer Book
-        </li>
-        <li>
-          NPPS 59 607
-        </li>
-        <li>
-          <a href="http://morse.fntsr.tw" target="_blank">
-            Morse Code
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/fntsrlike/edu-service-echelon-quota" target="_blank">
-            Echelon Quota Table of Education of SMS
-          </a>
-        </li>
-        <li>
-          <a href="http://163.17.100.89" target="_blank">
-            ICS2015: Conference Registry and Paper Review System
-          </a>
-        </li>
-        <li>
-          <a href="http://life.ncyu.refactor.tw/index.html" target="_blank">
-            An-Xin-Zai-Jia (安心在嘉)
-          </a>
-        </li>
-        <li>
-          Slack IRC Syncbot
-          <a class="github-icon" target="_blank" href="https://github.com/fntsrlike/slack-irc-syncbot" title="source code">
-            <i class="fa fa-github"></i>
-          </a>
-        </li>
-        <li>
-          Board Mangement System
-          <a class="github-icon" target="_blank" href="https://github.com/fntsrlike/boards_manager" title="source code">
-            <i class="fa fa-github"></i>
-          </a>
-        </li>
-        <li>
-          Appeal Managemen System
-          <a class="github-icon" target="_blank" href="https://github.com/fntsrlike/appeal_system">
-            <i class="fa fa-github"></i>
-          </a>
-        </li>
-      </ul>
-    </section>
-    <section>
-      <p>
-        There are also some project I contribute or hackathon.
-      </p>
-      <ul>
-        <li>
-          <a target="_blank" href="https://github.com/philipz/docker_practice">
-            《Docker —— 從入門到實踐­》正體中文版
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="http://laravel-taiwan.github.io/php-the-right-way">
-            PHP: The Right Way
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://github.com/raincole/isr">
-            ISR 2013: Burning_Civilization
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://github.com/Chatterbox-mirror/Chatterbox">
-            Chatterbox
-          </a>
-        </li>
-      </ul>
-    </section>
-    <section>
+    <p>
       You can see the details on my <router-link to="/collections">collections</router-link>.
-    </section>
+    </p>
   </section>
 </template>
 
