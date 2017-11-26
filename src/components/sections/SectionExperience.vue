5c71380a821263965030c5b475058c048eccd6e9
Ruoshi Lin <me@fntsr.tw>
2017-11-26 23:28:51 +0800

    Change experieces source from hard-code to json

--- src/components/sections/SectionExperience.vue
+++ src/components/sections/SectionExperience.vue
@@ -1,72 +1,36 @@
 <template>
   <section class="block">
     <h2>Experience</h2>
-    <p>
-      I have experience in community management, speech on conference, and teamwork.
-    </p>
     <ul>
-      <li>
-        <em>(Looking for work.)</em>
-      </li>
-      <li>
-        A member of
-        <a target="_blank" href="http://oolab.csie.ncu.edu.tw/wiki/index.php">OOLab</a> of
-        <a target="_blank" href="http://www.ncu.edu.tw/">NCU</a> (2014-2016)
-      </li>
-      <li>
-        A speaker at <a target="_blank" href="http://2015.phpconf.tw">PHPConf 2015</a>.
-        <a class="slide-icon" target="_blank" href="https://www.slideshare.net/ruoshiling/ship-your-projects-with-docker-ecosystem">
-          <i class="fa fa-slideshare"></i>
-        </a>,
-        <a class="video-icon" target="_blak" href="https://www.youtube.com/watch?v=B0Xw38EkFf8&feature=youtu.be">
-          <i class="fa fa-youtube-play"></i>
-        </a>
-      </li>
-      <li>
-        A speaker at <a target="_blank" href="http://phpconf.tw/2014/">PHPConf 2014</a>.
-        <a class="slide-icon" target="_blank" href="http://www.slideshare.net/ruoshiling/how-to-deploy-php-projects-with-docker">
-          <i class="fa fa-slideshare"></i>
-        </a>,
-        <a class="video-icon" target="_blak" href="https://www.youtube.com/watch?v=Y5Gvk0mutZ4&index=13&list=PLI5n7xV4X5qUlAlVnu9aajJeoFDc3mYzv">
-          <i class="fa fa-youtube-play"></i>
-        </a>
-      </li>
-      <li>
-        A speaker at <a target="_blank" href="http://nos.ncu.cc">Network & Open Source Club</a> of <a target="_blank"
-          href="http://www.ncu.edu.tw/">NCU</a>.
-        <a class="slide-icon" target="_blank" href="http://www.slideshare.net/ruoshiling/docker-48710374">
-          <i class="fa fa-slideshare"></i>
-        </a>,
-        <a class="ticket-icon" target="_blank" href="http://nos.kktix.cc/events/d1fe178c">
-          <i class="fa fa-ticket"></i>
-        </a>
-      </li>
-      <li>
-        A speaker at <a target="_blank" href="http://www.ntust.edu.tw/">Coding Club</a> of <a target="_blank" href="http://www.ntust.edu.tw/">NTUST</a>.
-        <a class="ticket-icon" target="_blank" href="http://ntust-coding-club.kktix.cc/events/ntust-docker">
-          <i class="fa fa-ticket"></i>
-        </a>
-      </li>
-      <li>
-        A speaker at <a target="_blank" href="http://www.yuntech.edu.tw/">YunTech</a>.
-        <a class="slide-icon" target="_blank" href="http://www.slideshare.net/ruoshiling/ss-48925329">
-          <i class="fa fa-slideshare"></i>
-        </a>
-      </li>
-      <li>
-        A 2014 summer inten of <a target="_blank" href="http://www.eztable.com/">EZTABLE</a>.
-      </li>
-      <li>
-        Founder of <a target="_blank" href="https://nchueecsec.github.io/">NCHU EECSeC</a> (Merged with <a target="_blank"
-          href="https://nchuit.github.io/">NCHUIT</a>)
+      <li v-for="(experience, index) in experiences" :key="experience.id">
+          <span v-html="experience.name.join(' ')"></span>
+          <a v-if="experience.links.slides"
+             class="slide-icon" target="_blank" v:href="collection.links.slides">
+            <i class="fa fa-slideshare"></i>
+          </a>
+          <a v-if="experience.links.video"
+             class="video-icon" target="_blank" v:href="collection.links.video">
+            <i class="fa fa-youtube-play"></i>
+          </a>
+          <a v-if="experience.links.ticket"
+             class="ticket-icon" target="_blank" v:href="collection.links.ticket">
+            <i class="fa fa-ticket"></i>
+          </a>
       </li>
     </ul>
   </section>
 </template>
 
 <script>
+import ExperiencesData from '../../data/dist/experiences.json'
+
 export default {
-  name: 'SectionExperience'
+  name: 'SectionExperience',
+  data () {
+    return {
+      experiences: ExperiencesData
+    }
+  }
 }
 </script>
 
