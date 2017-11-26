0b97a15128008e0cfeb2c47d0a30cd5367b8be02
Ruoshi Lin <me@fntsr.tw>
2017-11-27 00:46:53 +0800

    Add Organizations section

--- src/components/TheCurriculumVitae.vue
+++ src/components/TheCurriculumVitae.vue
@@ -8,6 +8,7 @@
   <section-professional></section-professional>
   <section-education></section-education>
   <section-experience></section-experience>
+  <section-organizations></section-organizations>
   <section-projects></section-projects>
   <section-awards></section-awards>
   <section-languages></section-languages>
@@ -18,6 +19,7 @@
 import SectionProfessional from './sections/SectionProfessional'
 import SectionEducation from './sections/SectionEducation'
 import SectionExperience from './sections/SectionExperience'
+import SectionOrganizations from './sections/SectionOrganizations'
 import SectionProjects from './sections/SectionProjects'
 import SectionAwards from './sections/SectionAwards'
 import SectionLanguages from './sections/SectionLanguages'
@@ -28,6 +30,7 @@ export default {
     'section-professional': SectionProfessional,
     'section-education': SectionEducation,
     'section-experience': SectionExperience,
+    'section-organizations': SectionOrganizations,
     'section-projects': SectionProjects,
     'section-awards': SectionAwards,
     'section-languages': SectionLanguages
---
+++ src/components/sections/SectionOrganizations.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="block">
+    <h2>Organizations & Communities</h2>
+    <ul>
+      <li>Host of Docker.Taipei </li>
+      <li>Student representative of Dept. of History, Congress, Student Government of NCHU </li>
+      <li>Minister of Dept. of Information Technology, Administrative Center, Student Government of NCHU </li>
+      <li>Member of Dept. of Secretary, Administrative Center, Student Government of NCHU</li>
+      <li>Vice President of Chinese medicine club, NCHU</li>
+      <li>Vice President of Chinese medicine club, NCHU </li>
+      <li>Activities Director of information clubs union winter camp </li>
+      <li>Administrator of BBS New Fantasy, WHSH</li>
+      <li>16th President of computer science club, WHSH</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SectionOrganizations'
+}
+</script>
+
+<style scoped>
+
+</style>
