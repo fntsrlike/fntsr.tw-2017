153ebe7a111732745e1d623b6f46af1242cb3de3
Ruoshi Lin <me@fntsr.tw>
2017-11-27 10:30:58 +0800

    Fixed excepted salary

--- src/components/sections/SectionAnnouncement.vue
+++ src/components/sections/SectionAnnouncement.vue
@@ -2,10 +2,10 @@
   <section class="block">
     <p>
       I am looking for the job.
-      Here is my <a href="https://www.dropbox.com/s/vv5axqrar4zvlbe/Resume.pdf?dl=1">résumé</a>.
+      Here is my <a href="https://www.dropbox.com/s/s5j3p94axf24rji/Resume.pdf?dl=0">résumé</a>.
     </p>
     <p>
-      My major is Web development and Software Engineering. My expected salary is at least $3,000/year.
+      My major is Web development and Software Engineering. My expected salary is at least $25,000/year.
     </p>
   </section>
 </template>
